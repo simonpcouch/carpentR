c46931ea16be136205e363b7b0ac246747f5e3de
Simon Couch <simonpatrickcouch@gmail.com>
2020-02-08 09:47:32 -0800

    fix indexing per interval

--- R/run_carpenter.R
+++ R/run_carpenter.R
@@ -9,17 +9,26 @@ run_carpenter <- function(x) {
   
   # preallocate some vectors
   x$p_results <- x$sa_results <- x$la_results <- x$z_results <- 
-    day <- achl <- bchl <- tchl <- zb <- rep(NA, x$n_intervals - 1)
+    achl <- bchl <- tchl <- zb <- rep(NA, x$n_intervals)
+  x$day <- 1:x$n_intervals
   x$x <- 1
+  
+  # initialize the first values of phosphorus, 
+  # small algae, large algae, and zooplankton
+  x$p_results[1] <- x$initial_p
+  x$sa_results[1] <- x$initial_sa
+  x$la_results[1] <- x$initial_la
+  x$z_results[1] <- x$initial_z
+  
   
   # triply nested loops.. sheesh (use this instead of the apply/map family
   # so that we don't have to pass values as function-level variables 
   # and since all vectors can be pre-allocated)
   for (i in 2:x$n_intervals) {
 
-    # reset the values at the beginning of each iteration
-    # initial concentrations of p, sa, la, and z
-    y <- list(x$initial_p, x$initial_sa, x$initial_la, x$initial_z)
+    # grab the most recent concentrations
+    y <- list(x$p_results[i-1], x$sa_results[i-1], 
+              x$la_results[i-1], x$z_results[i-1])
     
     for (j in 1:x$interval_length) {
       for (k in 1:x$n_steps_per_day) {
@@ -28,7 +37,7 @@ run_carpenter <- function(x) {
         x <- estimate_integrals(y, x)
         
         # check the bounds of the different variables
-        y <- lapply(list(y[[1]], y[[2]], y[[3]], y[[4]]), check_bounds)
+        y <- lapply(y, check_bounds)
         
         x$x <- round(x$x + x$h)
         x$tday <- x$x
@@ -44,7 +53,6 @@ run_carpenter <- function(x) {
       } # end of k-indexed loop
     } # end of j-indexed loop
     
-    day[i] <- x
     x$p_results[i] <- y[[1]]
     x$sa_results[i] <- y[[2]]
     x$la_results[i] <- y[[3]]
@@ -56,15 +64,15 @@ run_carpenter <- function(x) {
   for (i in 1:x$n_intervals) {
     
     # some calculations for algal chlorophyll
-    achl[i] <- x$g1 * y[[2]][i]
-    bchl[i] <- x$g2 * y[[3]][i]
+    achl[i] <- x$g1 * x$sa_results[i]
+    bchl[i] <- x$g2 * x$la_results[i]
     tchl[i] <- achl[i] + bchl[i]
     zb[i] <- x$z_results[i] / 0.018
 
   }
   
   # store the results as list elements
-  x[c("achl", "bchl", "tchl", "zb", "day")] <- list(achl, bchl, tchl, zb, day)
+  x[c("achl", "bchl", "tchl", "zb")] <- list(achl, bchl, tchl, zb)
 
   # return the list
   x
