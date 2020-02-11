21050e78a3aef09eee31d352cfde88f6fdf562e3
Simon Couch <simonpatrickcouch@gmail.com>
2020-02-11 12:55:57 -0800

    specify dependencies, add package-level docs

---
+++ R/carpentR.R
@@ -0,0 +1,13 @@
+#' Package: carpentR
+#'
+#' This R Package implements a model from Stephen R. Carpenter's 1992 
+#' paper on predicting lake algal blooms using plankton dynamics and other 
+#' variables, and was developed specifically for use in Sam Fey's Ecology 
+#' class at Reed College.
+#'
+#' @docType package
+#' @name carpentR
+#' @importFrom dplyr %>%
+"_PACKAGE"
+
+utils::globalVariables(c(".", "perttype", "day", "phosphorus"))
